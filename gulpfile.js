a35421821def4c1890b6006bb9b988a48ce907ff
birbjam <41635757+birbjam@users.noreply.github.com>
2019-07-12 11:54:25 -0700

    restart with gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp')
+
+/*
+    TOP LEVEL FUNCTIONS
+
+    gulp.task - define tasks
+    gulp.src - point to the files to use
+    gulp.dest - point to the folder to output
+    gulp.watch - watch files and folders for changes
+
+*/
+
+// Logs message
+gulp.task('message', function () {
+  return console.log('Gulp is running...')
+})
+
+// Copy all HTML files
+gulp.task('copyHtml', async function () {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', async function () {
+  return console.log('Gulp is running...')
+})
