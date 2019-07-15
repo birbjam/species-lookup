cb297d1107b729f3ffad0172504102ed9cd136ec
birbjam <41635757+birbjam@users.noreply.github.com>
2019-07-14 19:34:56 -0700

    set up basic query for testing

--- query.js
+++ query.js
@@ -1,6 +1,13 @@
-fetch(
-  "https://specieslookup.berkeley.edu/search_json/-123.61895787374944,39.724578563018255"
-).then(response => {
-  console.log(response);
-  return response.json();
-});
+var queryURL =
+  'https://specieslookup.berkeley.edu/search_json/-123.61895787374944,39.724578563018255'
+
+fetch(queryURL, {
+  method: 'GET'
+})
+  .then((result) => result.json())
+  .then((response) => {
+    // Testing response, getting the class of the first item from the json data
+    let heading = document.createElement('h1')
+    heading.innerText = `Class: ${response.species[0].class}`
+    document.querySelector('#data').appendChild(heading)
+  })
